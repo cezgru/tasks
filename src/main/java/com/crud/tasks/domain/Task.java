8ff45cc273c65576d89fbb735e8fecb36c01cb79
Cezary Gruszka <cezgru@gmail.com>
2018-11-21 13:13:52 +0100

    15.2

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -3,15 +3,48 @@ package com.crud.tasks.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.persistence.*;
 
+@Entity
 public class Task {
     private Long id;
-    private String string;
+    private String title;
     private String  content;
 
-    public Task(Long id, String string, String content) {
+    public Task(Long id, String title, String content) {
         this.id = id;
-        this.string = string;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Task() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "description")
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
         this.content = content;
     }
 }
